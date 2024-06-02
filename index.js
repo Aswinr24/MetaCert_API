22809e1f2f51ae080c7181c54092a1600a4e7305
R Aswin <aswinr242004@gmail.com>
2024-06-02 15:16:41 +0530

    5

--- index.js
+++ index.js
@@ -12,22 +12,19 @@ const cors = require('cors')
 
 const app = express()
 app.use(cors())
-const port = 8080
+const PORT = 5050
 
 const JWT = process.env.JWT_TOKEN
 
-// Set up EJS as the templating engine
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 app.engine('ejs', ejsmate)
 
-// Middleware setup
 app.use(express.static(path.join(__dirname, '/public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(methodoverride('_method'))
 
-// Ensure the uploads directory exists
 const uploadsDir = path.join(__dirname, 'uploads')
 if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir)
@@ -68,23 +65,20 @@ app.post('/upload', upload.single('file'), async (req, res) => {
       'https://api.pinata.cloud/pinning/pinFileToIPFS',
       formData,
       {
-        maxContentLength: Infinity, // Set maxContentLength instead of maxBodyLength
+        maxContentLength: Infinity,
         headers: {
-          Authorization: `Bearer ${JWT}`, // Use JWT for authorization
+          Authorization: `Bearer ${JWT}`,
           ...formData.getHeaders(),
         },
       }
     )
 
-    // Construct IPFS gateway link
     const ipfsHash = pinataResponse.data.IpfsHash
     console.log(pinataResponse)
     const pinataLink = `ipfs://${ipfsHash}`
 
-    // Clean up the uploaded file
     fs.unlinkSync(filePath)
 
-    // Create a JSON file with the specified format
     const jsonContent = JSON.stringify({
       name: req.body.sname + "'s " + req.body.title,
       description: req.body.desc,
@@ -93,17 +87,16 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     const jsonFilePath = path.join(uploadsDir, `${req.body.sname}.json`)
     fs.writeFileSync(jsonFilePath, jsonContent)
 
-    // Upload the JSON file to Pinata
     const jsonFormData = new FormData()
     jsonFormData.append('file', fs.createReadStream(jsonFilePath))
 
     const jsonPinataMetadata = JSON.stringify({
-      name: 'metadata.json', // JSON file metadata
+      name: 'metadata.json',
     })
     jsonFormData.append('pinataMetadata', jsonPinataMetadata)
 
     const jsonPinataOptions = JSON.stringify({
-      cidVersion: 1, // Use CID version 1
+      cidVersion: 1,
     })
     jsonFormData.append('pinataOptions', jsonPinataOptions)
 
@@ -111,18 +104,16 @@ app.post('/upload', upload.single('file'), async (req, res) => {
       'https://api.pinata.cloud/pinning/pinFileToIPFS',
       jsonFormData,
       {
-        maxContentLength: Infinity, // Set maxContentLength instead of maxBodyLength
+        maxContentLength: Infinity,
         headers: {
-          Authorization: `Bearer ${JWT}`, // Use JWT for authorization
+          Authorization: `Bearer ${JWT}`,
           ...jsonFormData.getHeaders(),
         },
       }
     )
 
-    // Clean up the JSON file
     fs.unlinkSync(jsonFilePath)
 
-    // Return the hash of the uploaded JSON file
     const jsonPinataLink = `https://gateway.pinata.cloud/ipfs/${jsonPinataResponse.data.IpfsHash}`
     res.json({ pinataLink, jsonPinataLink })
   } catch (error) {
@@ -131,8 +122,8 @@ app.post('/upload', upload.single('file'), async (req, res) => {
   }
 })
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
 
 module.exports = app
